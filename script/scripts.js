184156e9ddb4a05b2df0c2b4cd8e027854a44918
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2023-10-20 16:16:00 -0300

    Aula 04 - Trabalhando com Multithread

--- script/scripts.js
+++ script/scripts.js
@@ -14,19 +14,6 @@ const graficoParaDolar =  new Chart(graficoDolar, {
   },
 });
 
-setInterval(() => {
-  conectaAPI();
-}, 5000);
-
-async function conectaAPI() {
-  const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
-  const conectaTraduzido = await conecta.json();
-  let tempo = geraHorario();
-  let valor = conectaTraduzido.USDBRL.ask;
-  adicionarDados(graficoParaDolar, tempo, valor);
-  imprimeCotacao("dolar", valor);
-}
-
 function geraHorario() {
   let data = new Date();
   let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
@@ -38,5 +25,14 @@ function adicionarDados(grafico, legenda, dados) {
   grafico.data.datasets.forEach((dataset) => {
     dataset.data.push(dados);
   });
-  grafico.update()
+  grafico.update();
 }
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
+workerDolar.addEventListener("message", e => {
+  let tempo = geraHorario();
+  let valor = e.data.ask;
+  imprimeCotacao("dolar", valor);
+  adicionarDados(graficoParaDolar, tempo, valor);
+})
